bfcfbed42118ba84aba20af97b4e43dcd8ba31ae
omari <osmith5544@gmail.com>
2023-06-21 13:50:08 -0500

    Added CRUD Map

---
+++ data/myListModel.js
@@ -0,0 +1,16 @@
+const todoList =[
+  {
+    name: 'Study',
+    dueDate: '2023-04-02'
+  },
+
+  {
+    name: 'work',
+    dueDate: '2023-01-02'
+  },
+
+  {
+    name: 'play',
+    dueDate: '2023-02-22'
+  }
+];
---
+++ scripts/myListController.js
@@ -0,0 +1,13 @@
+
+try {
+  //CREATE ACTION: NONE
+
+  //RETRIEVE ACTIONS: renderStartUpView -> renderTodoListRows
+
+  //UPDATE ACTIONS: button event addTodo
+
+  //DELETE ACTIONS: button event deleteTodo 
+
+} catch (error) {
+  console.log(error);
+}
